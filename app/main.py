b69c6627b56bf26503f3c1cd1810b836480b904b
saptadi73 <saptadi.nurfarid@gmail.com>
2026-01-03 10:30:31 +0700

    perbaikan peta santri dan pondok pesantren

--- app/main.py
+++ app/main.py
@@ -57,7 +57,8 @@ app.add_middleware(
         "http://localhost:4000",
         "http://127.0.0.1:4000",
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://santri.gagakrimang.web.id"
     ],
     allow_credentials=True,
     allow_methods=["*"],
